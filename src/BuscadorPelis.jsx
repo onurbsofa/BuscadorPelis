8ee028bf2ee492b0a4654664dcb1ec7a6d145315
BoFo <brunofazio466@gmail.com>
2023-08-31 11:26:44 -0300

    first commit

---
+++ src/BuscadorPelis.jsx
@@ -0,0 +1,54 @@
+export const BuscadorPelis = () => {
+
+  const [busqueda, setBusqueda] = useState('')
+  const [peliculas, setPeliculas] = useState([])
+
+  const urlDB = 'https://api.themoviedb.org/3/search/movie'
+  const API_key = '697a7a62b6c71e3353796490333311d4'
+
+
+  const handleInputChange = (e) => {
+    setBusqueda(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchPeliculas()
+  }
+
+  const fetchPeliculas = async () => {
+    try{
+      const response = await fetch(`${urlBase}?query=${busqueda}&api_key=${API_KEY}`)
+      const data = await response.json()
+    }
+
+  return (
+    <div className="container">
+
+      <h1 className="title">Buscador de Películas</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Escribí una película"
+          value={busqueda}
+          onChange={handleInputChange}
+        />
+        <button type="submit" className="search-button">Buscar</button>
+      </form>
+
+
+      <div className="movie-list">
+        {peliculas.map((pelicula) => (
+          <div key={pelicula.id} className="movie-card">
+            <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} alt={pelicula.title} />
+            <h2>{pelicula.title}</h2>
+            <p>{pelicula.overview}</p>
+          </div>
+
+        ))}
+
+      </div>
+
+    </div>
+  )
+}
